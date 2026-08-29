5e290c3ce4c503cd0215885bcf988ec568a89b5f
picky232 <mistick0215@gmail.com>
2026-08-29 21:13:59 +0900

    recursion complete

--- week2/1. basic/05_recursion.py
+++ week2/1. basic/05_recursion.py
@@ -54,10 +54,13 @@ def fibonacci(n):
     """
     # TODO: base case를 작성하세요
     # n이 0이면 0, n이 1이면 1 반환
-    
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    return fibonacci(n-1)+fibonacci(n-2)
     
     # TODO: recursive case를 작성하세요
-    pass
 
 # 테스트 케이스
 if __name__ == "__main__":
