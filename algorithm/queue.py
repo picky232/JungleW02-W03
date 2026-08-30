dfba7cc4db943030bd36397e6bfba04014a70571
picky232 <mistick0215@gmail.com>
2026-08-30 23:41:26 +0900

    주석 달기

--- algorithm/queue.py
+++ algorithm/queue.py
@@ -4,29 +4,30 @@ class queue:
         self.front = 0 # 추출할 데이터 위치
         self.rear = 0 # 삽입이 일어나는 위치
 
-    def enqueue(self, value):
+    def enqueue(self, value): # 원소 삽입
         self.data.append(value)
-        self.rear += 1
+        self.rear += 1 # 삽입 위치 증가
 
-    def dequeue(self):
-        out_data = self.data[self.front]
-        self.front += 1
+    def dequeue(self): # 원소 추출
+        out_data = self.data[self.front] # 추출한 데이터
+        self.front += 1 # 추출할 위치 증가
         return out_data
 
-    def isEmpty(self):
-        return bool(self.data)
+    def isEmpty(self): # 비엇는지 확인
+        return bool(self.data) # 확인후 bool 연산자 반환
 
-    def peak(self):
+    def peak(self): # 맨앞 값 반환
         return self.data[self.front]
 
-    def clear(self):
+    def clear(self): # 큐 초기화
         self.data = []
         self.front = 0
         self.rear = 0
 
-    def show(self):
+    def show(self): # 큐 보여주기 - f~r까지 
         for i in range(self.front, self.rear):
             print(self.data[i], end=" ")
+        print()
 
 
 q1 = queue()
