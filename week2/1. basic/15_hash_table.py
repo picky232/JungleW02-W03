29f4182b6246856aa11626ebd387219d9290117f
picky232 <mistick0215@gmail.com>
2026-09-01 13:59:40 +0900

    hash table complete

--- week2/1. basic/15_hash_table.py
+++ week2/1. basic/15_hash_table.py
@@ -41,11 +41,13 @@ def manage_grades(students):
         평균, 최고점 학생 이름, 최고점
     """
     # TODO: 평균 점수 계산
-    pass
+    names = list(students.keys())
+    average = sum(list(students.values()))/len(students)
+    top_student = max(students, key=students.get)
+    top_score = students[top_student]
     
     
     # TODO: 최고 점수 학생 찾기
-    pass
     
     return average, top_student, top_score
 
@@ -61,7 +63,10 @@ def find_student_score(students, name):
         점수 (없으면 None)
     """
     # TODO: students에서 name 찾기
-    pass
+    if name not in students:
+        return None
+    else:
+        return students[name]
 
 # 테스트 케이스
 if __name__ == "__main__":
