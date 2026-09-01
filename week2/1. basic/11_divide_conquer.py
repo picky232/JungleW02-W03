6206c0a43a86e0d4f51be314bbad8735f2a7c454
picky232 <mistick0215@gmail.com>
2026-09-01 14:49:47 +0900

    divide_conquer complete

--- week2/1. basic/11_divide_conquer.py
+++ week2/1. basic/11_divide_conquer.py
@@ -36,19 +36,24 @@ def find_max_divide_conquer(arr, left, right):
         최댓값
     """
     # TODO: base case - 원소가 하나면 그 값 반환
-    pass
+    if len(arr) == 1:
+        return arr
     
     # TODO: 중간 지점 계산
-    pass
+    mid = (left+right)//2
     
     # TODO: 왼쪽 절반의 최댓값
-    pass
-        
+    l_max = max(arr[left:mid])
+    left_arr = arr[left:mid]
     # TODO: 오른쪽 절반의 최댓값
-    pass
-    
+    r_max = max(arr[mid:right+1])
+    right_arr = arr[mid:right+1]
+
     # TODO: 둘 중 큰 값 반환
-    pass
+    if l_max > r_max:
+        return find_max_divide_conquer(left_arr, 0, mid)
+    else:
+        return find_max_divide_conquer(right_arr, 0, mid+1)
 
 # 테스트 케이스
 if __name__ == "__main__":
