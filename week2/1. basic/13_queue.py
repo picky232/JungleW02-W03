f886f47e9b09b958f5a81b5fdd2cd11776691d50
picky232 <mistick0215@gmail.com>
2026-09-01 12:48:35 +0900

    queue - complete

--- week2/1. basic/13_queue.py
+++ week2/1. basic/13_queue.py
@@ -38,15 +38,17 @@ def process_print_queue(jobs):
     """
     # TODO: deque로 큐 생성
     queue = deque(jobs)
-    
-    processed = []
+    processeds = []
     
     # TODO: 큐가 비어있지 않은 동안 반복
     ## 큐에서 작업 꺼내기
     ## 작업 처리 (출력 및 리스트에 추가)
-    pass
-    
-    return processed
+    while queue:
+        data = queue.popleft()
+        print("처리:", data)
+        processeds.append(data)
+        
+    return processeds
 
 # 테스트 케이스
 if __name__ == "__main__":
