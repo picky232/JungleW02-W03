6e7c8470b9d1bbc87aee06d9ce8cdb89abcfe123
picky232 <mistick0215@gmail.com>
2026-09-04 10:09:27 +0900

    graph - complete

--- week3/1. basic/03_graph_basic.py
+++ week3/1. basic/03_graph_basic.py
@@ -41,13 +41,26 @@ def create_graph(vertices, edges, directed=False):
         그래프 딕셔너리
     """
     # TODO: 빈 그래프 초기화
-    pass
+    graph = {}
     
     # TODO: 간선 추가
     ## 간선 추가 (u에서 v로)
     ## 무방향 그래프면 반대 방향도 추가
-    pass
-    
+    if not directed:
+        for i, j in edges:
+            if i not in graph:
+                graph.update({i:[]})
+            if j not in graph:
+                graph.update({j:[]})
+            graph[i].append(j)
+            graph[j].append(i)
+    else:
+        for i,j in edges:
+            if i not in graph:
+                graph.update({i:[]})
+            if j not in graph:
+                graph.update({j:[]})
+            graph[i].append(j)
     return graph
 
 # 테스트 케이스
