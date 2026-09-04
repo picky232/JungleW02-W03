9d3fa2acf5c3a4087cc8668dd8ecf7f9e8aeb2f1
picky232 <mistick0215@gmail.com>
2026-09-04 10:35:40 +0900

    bfs - 구현 완벽 X 다듬어야함

--- week3/1. basic/04_bfs.py
+++ week3/1. basic/04_bfs.py
@@ -47,14 +47,23 @@ def bfs(graph, start):
     
     # TODO: 큐 생성 및 시작 정점 추가
     ## 방문한 정점 집합
-    pass
+    que = deque([start])
+    # print(que)
 
     # TODO: 큐가 빌 때까지 반복
     ## 큐에서 정점 꺼내기
     ## 인접한 정점들 확인
     ## 방문하지 않은 정점이면 큐에 추가
-    pass
-    
+    while que:
+        # print("que: ",que)
+        # print("visitied: ",visited)
+        out = que.popleft()
+        # print(out)
+        for i in graph[out]:
+            if i not in visited:
+                que.append(i)
+        if out not in visited:
+            visited.append(out)
     return visited
 
 # 테스트 케이스
