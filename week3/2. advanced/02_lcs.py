323ed190911aa90f3e9235c1b507fd4083316b3a
picky232 <mistick0215@gmail.com>
2026-09-07 10:30:44 +0900

    complete

--- week3/2. advanced/02_lcs.py
+++ week3/2. advanced/02_lcs.py
@@ -41,7 +41,7 @@ def lcs_length(s1: str, s2: str) -> int:
         return 0
     # TODO: (len(s1)+1) x (len(s2)+1) 크기의 2차원 dp 배열을 0 으로 초기화
     dp = [[0]*(len(s2)+1) for _ in range(len(s1)+1)]
-    print(dp)
+    # print(dp)
     # TODO: 이중 반복문으로 점화식에 따라 dp 채우기
     for i in range(1,len(s1)+1):
       for j in range(1, len(s2)+1):
@@ -49,7 +49,7 @@ def lcs_length(s1: str, s2: str) -> int:
           dp[i][j] = dp[i-1][j-1] + 1
         else:
           dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-    print(dp)
+    # print(dp)
     # TODO: dp[len(s1)][len(s2)] 반환
     return dp[len(s1)][len(s2)]
 
